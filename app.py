4cb84f0a0901d001a1196dfb43b8d78c966c8c94
Yvan-BM <yvan.t.biakeu@aims-senegal.org>
2022-12-07 22:08:17 +0000

    first commit

--- app.py
+++ app.py
@@ -1,26 +1,57 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
 import torch
+# import convert
+# torch.multiprocessing.set_start_method('spawn', force=True)
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
+# transformers.models.gptj.modeling_gptj.GPTJBlock = convert.GPTJBlock
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# device = "cpu"
+
 def init():
     global model
+    global tokenizer
     
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    print("Tokenizer loading on cpu...")
+    tokenizer = AutoTokenizer.from_pretrained("Cedille/fr-boris")
+    print("done")
+
+    print("Model loading on cpu...")
+    model = transformers.AutoModelForCausalLM.from_pretrained("Cedille/fr-boris")
+    print("done")
+    
+    if device == "cuda:0":
+        print("Model passing on gpu...")
+        model.to(device)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
+    global tokenizer
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
     
+    print("request tokenize")
+    input_tokens = tokenizer(prompt, return_tensors='pt')
+    input_tokens = {key: value.to(device) for key, value in input_tokens.items()}
+    
     # Run the model
-    result = model(prompt)
+    print("request generate")
+    output = model.generate(**input_tokens, min_length=18, max_length=20, do_sample=True)
+    print("done")
+    
+    # Decode output token
+    print("request decode")
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print("done")
+    
+    result = {"output": output_text}
 
     # Return the results as a dictionary
     return result
--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 
 import requests
 
-model_inputs = {'prompt': 'Hello I am a [MASK] model.'}
+model_inputs = {'prompt': 'Je suis un jeune étudiant en'}
 
 res = requests.post('http://localhost:8000/', json = model_inputs)
 
